cecb738171a5326a6e0626bcaa98a5e4cf0dcc5d
Salomão Gonçalves Calheiros <salomao.calheiros@gmail.com>
2024-03-21 18:26:20 -0400

    schema notification

--- src/notification/interfaces/notification.schema.ts
+++ src/notification/interfaces/notification.schema.ts
@@ -1,4 +1,6 @@
 import * as mongoose from 'mongoose';
+import * as moment from 'moment';
+moment.locale('pt-br');
 
 export const NotificationSchema = new mongoose.Schema({
     sensor: { 
@@ -8,7 +10,10 @@ export const NotificationSchema = new mongoose.Schema({
     type: {type: String, required: true},
     value_limite:{ type: Number, required: true},
     value: { type: Number, required: true},
-    emails: Array<{type: string }>
+    emails: Array<{type: string }>,
+    week: { type: Number, default: moment().isoWeek()},
+    month: { type: Number, default: moment().month()+1},
+    year: { type: Number, default: moment().year()},
 
 }, { timestamps: true , collection: 'notification'});
 
